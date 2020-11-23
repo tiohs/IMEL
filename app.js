abbda1a9b554cd81f3e8523f9d16facefebd080c
Hamilton Massango <hamiltonsilva55@gmail.com>
2020-11-23 19:33:02 +0100

    add Session

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const express = require("express");
 var session = require('express-session');
-// var MySQLStore = require('express-mysql-session')(session);
-// var cookie = require('cookie-parser');
-// var options = {
-// 	host: 'localhost',
-// 	port: 3306,
-// 	user: 'root',
-// 	database: 'upra'
-// };
+var MySQLStore = require('express-mysql-session')(session);
+var cookie = require('cookie-parser');
+var options = {
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	database: 'upra'
+};
 
-// var sessionStore = new MySQLStore(options);
+var sessionStore = new MySQLStore(options);
 
 // const bodyParser = require('body-parser');
 const app = express();
@@ -24,13 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Session 
 
-// app.use(session({
-// 	key: 'session_cookie_name',
-// 	secret: 'session_cookie_secret',
-// 	store: sessionStore,
-// 	resave: false,
-// 	saveUninitialized: false
-// }));
+app.use(session({
+	key: 'session_cookie_name',
+	secret: 'session_cookie_secret',
+	store: sessionStore,
+	resave: false,
+	saveUninitialized: true
+}));
 
 // const routeAluno = require('./routes/alunos.routes');
 // const routeAdmin = require('./routes/admin.routes');
@@ -40,7 +40,6 @@ const routerCoordenador = require('./routes/coordenador.routes');
 // app.use("/admin", routeAdmin);
 // app.use(routeAluno);
 app.use('/cordenacao', routerCoordenador);
-
 app.listen(3000, ()=> {
 	console.log('Server on port 3000');
 });
---
+++ routes/colaborador.routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const controllersColaborador = require('../controllers/colaborador');
+
+module.exports = router;

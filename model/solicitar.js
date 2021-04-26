5afb0ac5aca58527fc88f5b834393017754f7a16
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-04-26 11:16:17 +0100

    create lista de troca

--- model/solicitar.js
+++ model/solicitar.js
@@ -41,14 +41,27 @@ class Solicitar {
   static indexByTwo(idPost) {
     return knex
     .select('solicitartroca.*', 
-    'aluno1.id as idAluno',
+    'aluno1.id as idAluno1',
     'aluno1.photoAvatar as Avatar1',
     'aluno1.nome as nome1',
-    'aluno1.sala as sala1'
+    'aluno1.sala as sala1',
+    'curso1.nomeCurso as nomeCurso1',
+    'turma1.nome as nomeTurma1',
+    'aluno2.id as idAluno2',
+    'aluno2.photoAvatar as Avatar2',
+    'aluno2.nome as nome2',
+    'aluno2.sala as sala2',
+    'curso2.nomeCurso as nomeCurso2',
+    'turma2.nome as nomeTurma2'
     )
     .from('solicitartroca')
     .whereRaw(` solicitartroca.id   = ${ idPost }`)
-    .join('aluno as aluno1', 'solicitartroca.idUser', '=',  'aluno1.id');
+    .join('aluno as aluno1', 'solicitartroca.idUser', '=',  'aluno1.id')
+    .join('curso as curso1', 'aluno1.idCurso', '=' , 'curso1.id')
+    .join('turma as turma1', 'aluno1.idTurma', '=', 'turma1.id')
+    .join('aluno as aluno2', 'solicitartroca.interessado', '=',  'aluno2.id')
+    .join('curso as curso2', 'aluno2.idCurso', '=' , 'curso2.id')
+    .join('turma as turma2', 'aluno2.idTurma', '=', 'turma2.id');
   }
 }
 
